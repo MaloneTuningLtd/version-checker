ba6812372c1991348bf05e859064475c7d653b2d
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-07-19 16:25:06 -0700

    refactor: remove TODO

--- src/events/bootstrap.js
+++ src/events/bootstrap.js
@@ -4,7 +4,7 @@ const { updated } = require('./listeners/VersionEventListener');
 const bootVersionEventEmitter = () => {
   const vee = new VersionEventEmitter();
 
-  // TODO: specify on event listener handlers here.
+  // Specify on event listener handlers here.
   vee.on('updated', updated);
 
   return vee;
