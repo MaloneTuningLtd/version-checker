18ef0e9a93eca3ea0a425eb42dd65d5db63c90bb
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-07-24 16:55:43 -0700

    fix: move versions.json to data folder

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
   versionsPath: process.env.VERSION_PATH ||
-    path.join(__dirname, './versions.json'),
+    path.join(__dirname, 'data', 'versions.json'),
 };
