1af8e9acd3a3080825df51d01be3877bc3e00e81
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-09-21 16:05:53 -0700

    feat: change reading sources off of json file

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,9 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
 const { githubRepo } = require('./common/github.provider');
 
-module.exports = [
-  githubRepo('gradle', 'gradle/gradle'),
-  githubRepo('composer', 'composer/composer'),
-  githubRepo('phpunit', 'sebastianbergmann/phpunit'),
-  githubRepo('npm', 'npm/cli'),
-  githubRepo('yarn', 'yarnpkg/yarn'),
-];
+const readSourcesFile = (sourceName) => {
+  try {
+    const sourcePath = path.resolve(__dirname, '../../config/sources.json')
+    const file = fs.readFileSync(sourcePath, 'utf8')
+
+    const sources = JSON.parse(file);
+
+    // TODO: fixme
+    return sources[sourceName]
+  } catch (e) {
+    console.error(e)
+    return null;
+  }
+};
+
+module.exports = () => {
+  const github = readSourcesFile('github')
+
+  return github.map(g => {
+    const [org, name] = g.split('/')
+
+    return githubRepo(name, g)
+  })
+}
